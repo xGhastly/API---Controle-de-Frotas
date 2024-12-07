2dccd0e7324b91f2b8b99c7961ab114f4bc2ce1c
João Augusto <joaoaugustozm@hotmail.com>
2024-12-07 13:35:13 -0300

    Continue trip controller

--- src/controllers/trips/CreateTripController.ts
+++ src/controllers/trips/CreateTripController.ts
@@ -1,15 +1,24 @@
 import { NextFunction, Request, Response } from "express";
 
 class CreateTripController {
+    constructor(private readonly tripService: CreateTripService) { }
     async handle(req: Request, res: Response, next: NextFunction) {
-        const { origin, destination, passengers, date, distanceKm, driverId, vehicleId } = req.body as {
-            origin: number;
-            destination: string;
-            passengers: number;
-            date: string;
-            distanceKm: number;
-            driverId: number;
-            vehicleId: number;
+        try {
+            const { origin, destination, passengers, date, distanceKm, driverId, vehicleId } = req.body as {
+                origin: number;
+                destination: string;
+                passengers: number;
+                date: string;
+                distanceKm: number;
+                driverId: number;
+                vehicleId: number;
+            }
+            const createdTrip = await this.tripService.createTrip(origin, destination, passengers, date, distanceKm, driverId, vehicleId)
+            res.status(403).send(createdTrip)
+
+        } catch (error) {
+            next(error)
         }
+
     }
 }
