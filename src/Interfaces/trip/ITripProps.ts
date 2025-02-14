b20351a6385889338911209e5fa44da3ee77e613
João Augusto <joaoaugustozm@hotmail.com>
2025-02-13 22:07:26 -0300

    adicionar arquivo trip

---
+++ src/Interfaces/trip/ITripProps.ts
@@ -0,0 +1,10 @@
+interface ITripProps {
+    origin: string;
+    destination: string;
+    passengers: number;
+    date: string;
+    distanceKm: number;
+    driverId: number;
+    vehicleId: number;
+}
+export { ITripProps };
